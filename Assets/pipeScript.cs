212c6efe168833b398f56e6dff2313d40a591aa7
widianakomang31-dotcom <widianakomang31@gmail.com>
2026-01-19 00:02:21 +0800

    project start

---
+++ Assets/pipeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeScript : MonoBehaviour
+{
+
+    float nilaiRandom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nilaiRandom = Random.Range(-2.53f, 1.26f);
+        transform.position = new Vector2(transform.position.x, nilaiRandom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Vector2.left * 100, Time.deltaTime * 5);
+    }
+}
